d310f8907e60a63448a5b1288ae564abe75863f2
paybey2-crypto <paybey2@gmail.com>
2025-11-10 03:30:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,3 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Aplikacija radi!"
-
-if __name__ == '__main__':
-    app.run(debug=True)
-    from flask import render_template
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
+@app.route('/test')
+def test():
+    return "Test stranica"
