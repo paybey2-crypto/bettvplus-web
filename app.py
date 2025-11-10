dba95a76e3ecbb2d02448108832122a99874465d
Bet TV Plus <paybey2@gmail.com>
2025-11-10 21:57:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
+from flask import Flask, render_template, request, redirect, url_for
 
-/project_folder/
-├── app.py
-├── BETTVPLUS-PRO.apk
-└── templates/
-    ├── index.html
-    ├── success.html
-    └── error.html
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    mac = request.form.get('mac')
+    code = request.form.get('code')
+
+    # Provjera MAC adrese i koda
+    if mac == '59:d9:5d:f3:8c:cf' and code == '38713':
+        return render_template('success.html')
+    else:
+        return render_template('error.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
