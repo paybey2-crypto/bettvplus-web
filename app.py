4640846a2c88adf6303daedd5d6aedff2e06afee
Bet TV Plus <paybey2@gmail.com>
2025-11-10 20:05:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # ovdje ide tvoj login/aktivacijski kod
+        return "Prijava uspješna"
+    else:
+        return render_template('index.html')
