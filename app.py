e3de6a96bd5e46310130a20491209e84f1fb9ebf
Bet TV Plus <paybey2@gmail.com>
2025-11-10 21:59:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,33 @@
-from flask import Flask, render_template, request, redirect, url_for
+
+from flask import Flask, render_template, request, send_from_directory
 
 app = Flask(__name__)
 
+# Glavna stranica (login forma)
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Obrada prijave
 @app.route('/login', methods=['POST'])
 def login():
     mac = request.form.get('mac')
     code = request.form.get('code')
 
-    # Provjera MAC adrese i koda
+    # Jednostavna provjera
     if mac == '59:d9:5d:f3:8c:cf' and code == '38713':
         return render_template('success.html')
     else:
         return render_template('error.html')
+
+# Ruta za preuzimanje APK fajla
+@app.route('/download')
+def download_apk():
+    return send_from_directory(
+        directory='.', 
+        path='BETTVPLUS-PRO.apk', 
+        as_attachment=True
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
