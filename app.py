ac60b84d1df7eeeb54a164c2f33c748825aff4d7
paybey2-crypto <paybey2@gmail.com>
2025-11-10 03:18:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,3 @@
-import os
-import stripe
-from flask import Flask, render_template, request, redirect, jsonify
-import sqlite3
-
-app = Flask(__name__)
-
-# Učitaj Stripe ključeve iz .env fajla
-stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
-STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY")
-
-# Inicijalizacija baze
-def init_db():
-    conn = sqlite3.connect('data.db')
-    c = conn.cursor()
-    c.execute("""
-        CREATE TABLE IF NOT EXISTS activations (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            mac TEXT UNIQUE
-        )
-    """)
-    conn.commit()
-    conn.close()
-
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/')
+def home():
+    return "Aplikacija radi!"
