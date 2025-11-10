573e855c574356d3b804be3b8b7ae8e83d2ea8c1
Bet TV Plus <paybey2@gmail.com>
2025-11-10 21:04:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 @app.route('/login', methods=['POST'])
 def login():
     mac = request.form.get('mac')
@@ -8,8 +16,6 @@ def login():
         return render_template('error.html', message="Nedostaju podaci za prijavu.")
 
     return render_template('success.html', mac=mac)
-    @app.route('/')
-def index():
-    return render_template('index.html')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
