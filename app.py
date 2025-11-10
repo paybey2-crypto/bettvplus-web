e08fc81a18b25fbc5d43ea1fbbbc3b2279fa1127
Bet TV Plus <paybey2@gmail.com>
2025-11-10 21:48:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,8 @@
-print("Primljen zahtjev za login")
-print("MAC:", mac, "CODE:", code)
-from flask import Flask, render_template, request
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/login', methods=['POST'])
-def login():
-    mac = request.form.get('mac')
-    code = request.form.get('code')
-
-    if not mac or not code:
-        return render_template('error.html', message="Nedostaju podaci za prijavu.")
-
-    return render_template('success.html', mac=mac)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+/project_folder/
+├── app.py
+├── BETTVPLUS-PRO.apk
+└── templates/
+    ├── index.html
+    ├── success.html
+    └── error.html
