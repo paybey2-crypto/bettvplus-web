de1f0a2351d56fe04092a6bb34e3a468eafbd431
Bet TV Plus <paybey2@gmail.com>
2025-11-10 19:23:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run()
+@app.route('/login')
+def login():
+    return "<h2>Login stranica je u izradi.</h2>"
