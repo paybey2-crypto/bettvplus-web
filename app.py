72642b56b6c25410c38712c4a2f5e3a192ac2ef1
Bet TV Plus <paybey2@gmail.com>
2025-11-15 22:02:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,11 @@
-from flask import Flask, render_template, request
+@app.route('/play')
+def play():
+    username = request.args.get("username")
+    password = request.args.get("password")
 
-app = Flask(__name__)
+    if not username or not password:
+        return "Nedostaje username ili password!", 400
 
-# ADMIN LOZINKE
-ADMIN_USERNAME = "admin"
-ADMIN_PASSWORD = "Livnjak1978@@"
+    stream_url = f"http://anotv.org:80/get.php?username={username}&password={password}&type=m3u_plus&output=ts"
 
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-@app.route('/admin', methods=['GET', 'POST'])
-def admin():
-    if request.method == 'POST':
-        username = request.form.get("username")
-        password = request.form.get("password")
-
-        if username == ADMIN_USERNAME and password == ADMIN_PASSWORD:
-            return render_template("admin.html")
-        else:
-            return "Pogrešan username ili password!", 401
-
-    return render_template("login.html")
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    return render_template("player.html", stream_url=stream_url)
