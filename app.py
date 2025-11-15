aa877d95994ffc184ca1587ed033613cecdfa2ba
Bet TV Plus <paybey2@gmail.com>
2025-11-15 21:09:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,36 @@
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
+
+# ---------------------------
+# HOME PAGE
+# ---------------------------
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+# ---------------------------
+# ADMIN PANEL (LOGIN)
+# URL: /admin-LIVNJAK1978
+# ---------------------------
+@app.route("/admin-LIVNJAK1978", methods=["GET", "POST"])
+def admin():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        # Možeš promijeniti login podatke
+        if username == "admin" and password == "Livnjak1978@@":
+            return "Uspješno logovan!"
+
+        return "Pogrešan username ili password!"
+
+    return render_template("admin.html")
+
+
+# ---------------------------
+# STARTER
+# ---------------------------
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
