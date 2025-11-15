22290d12d7a5d607b61d87f9eb0ece46930b2939
Bet TV Plus <paybey2@gmail.com>
2025-11-15 14:36:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,4 +15,6 @@ def login():
     return f"MAC: {mac}<br>PIN: {pin}<br>DNS: {dns}"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
