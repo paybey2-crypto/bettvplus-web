64669f8b9da31ef67ae040a940cefe882f5d034d
Bet TV Plus <paybey2@gmail.com>
2025-11-16 01:48:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -25,3 +25,6 @@ def activate():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
+    @app.route("/activate/<mac>")
+def activate(mac):
+return render_template("activate.html", mac=mac)
