4fb946285ca062de1019c76815e7638ccd205b32
transfer-app <paybey2@gmail.com>
2025-11-27 12:56:29 +0100

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,1 +1,11 @@
+import mysql from "mysql2";
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+}).promise();
+
+export default pool;
 
