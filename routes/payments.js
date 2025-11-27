a0fb1bc8d8b89eabdc14f34ffeda5ffd41c122df
transfer-app <paybey2@gmail.com>
2025-11-27 15:30:08 +0100

    Update payments.js

--- routes/payments.js
+++ routes/payments.js
@@ -1,1 +1,11 @@
+import express from "express";
+const router = express.Router();
+
+// Primer Stripe webhook rute
+router.post("/webhook", (req, res) => {
+    console.log("Stripe webhook received:", req.body);
+    res.status(200).send("Webhook received");
+});
+
+export default router;
 
