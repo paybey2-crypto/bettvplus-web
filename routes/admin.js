f3b41f8769ea92fdc7d4b363e6dab8fba9232039
transfer-app <paybey2@gmail.com>
2025-11-27 15:29:08 +0100

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,1 +1,10 @@
+import express from "express";
+const router = express.Router();
+
+// Primer admin rute
+router.get("/", (req, res) => {
+    res.json({ message: "Admin route" });
+});
+
+export default router;
 
