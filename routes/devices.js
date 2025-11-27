e091aa34298d89f6b349f4e7cf36eb0ff0fe53dd
transfer-app <paybey2@gmail.com>
2025-11-27 15:29:45 +0100

    Update devices.js

--- routes/devices.js
+++ routes/devices.js
@@ -1,1 +1,10 @@
+import express from "express";
+const router = express.Router();
+
+// Primer rute
+router.get("/", (req, res) => {
+    res.json({ message: "Devices route" });
+});
+
+export default router;
 
